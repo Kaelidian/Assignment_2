bb8cad063bb464a1d3108613717897d9202c719d
nefar <nefar@DESKTOP-CGDP01C>
2022-11-25 15:52:24 -0600

    Add project files.

---
+++ Assignment_2/LibraryLoanItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2
+{
+    internal class LibraryLoanItem
+    {
+        protected string callNumber; // Format "100 GAD"
+        protected string title;
+        protected string author;
+        protected int copies;
+        protected int availableCopies;
+        protected int loanPeriod;
+        protected int maxRenewals;
+        protected int timesRenewed;
+
+        public LibraryLoanItem(string callNumber, string title, string author, int copies, int loanPeriod, int maxRenewals)
+        {
+
+
+            this.callNumber = callNumber;
+            this.title = title;
+            this.author = author;
+            this.copies = copies;
+            this.loanPeriod = loanPeriod;
+            this.maxRenewals = maxRenewals;
+        
+        }
+    }
+}
